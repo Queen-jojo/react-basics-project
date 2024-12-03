9b291146e48511065151ee35a2d605306d3bc942
Jo-Anne Snip <tali-surimeid@hotmail.com>
2024-12-03 15:28:41 +0100

    Search Icon deleted

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,26 +1,17 @@
-import {
-  Input,
-  InputGroup,
-  InputLeftElement,
-  SearchIcon,
-} from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 export const SearchBar = ({ onSearch }) => {
   const handleSearch = (event) => {
+    console.log("event:", event);
     const query = event.target.value;
     onSearch(query);
   };
 
   return (
-    <InputGroup>
-      <InputLeftElement pointerEvents="none">
-        <SearchIcon color="gray.300" />
-      </InputLeftElement>
-      <Input
-        type="text"
-        placeholder="Search recipes..."
-        onChange={handleSearch}
-      />
-    </InputGroup>
+    <Input
+      type="text"
+      placeholder="Search recipes..."
+      onChange={handleSearch}
+    />
   );
 };
